42a0e900632c14c8cb169039c689d068e0a63e40
Yeferson Cronos <yefersonyscc2@gmail.com>
2022-09-10 17:06:39 -0500

    potencial operator, array includes

---
+++ src/es7/00-operador-potencial.js
@@ -0,0 +1,7 @@
+//Usually we use Math.pow to do exponentiation
+let num = Math.pow(3, 2);
+console.log(num);
+
+//With es7 we use ** for the same case
+let num2 = 3 ** 2;
+console.log(num2);
---
+++ src/es7/01-array-includes.js
@@ -0,0 +1,8 @@
+//This method return a boolean (true or false), if an item exists in an array
+let arr = [1, 5, 34, 2, 75, 23];
+console.log(arr.includes(3));
+console.log(arr.includes(34));
+
+let arrString = ['Hola', 'Hello', 'Hi', 'Hallo'];
+console.log(arrString.includes('Oi'));
+console.log(arrString.includes('Hello'));
